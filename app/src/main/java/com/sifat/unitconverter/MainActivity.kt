bf7231fd2550593789d68de877b433f95781b628
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-02-05 22:13:34 +0600

    Spinner

--- app/src/main/java/com/sifat/unitconverter/MainActivity.kt
+++ app/src/main/java/com/sifat/unitconverter/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.sifat.unitconverter
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sifat.unitconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val options = listOf(
+            "Height: Feet \\u2194 Meter",
+            "Length: Meter \\u2194 Kilometer",
+            "Weight: Kilogram \\u2194 Pound",
+            "Temperature: Celsius \\u2194 Fahrenheit"
+        )
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinner.adapter = adapter
     }
 }
